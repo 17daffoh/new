2586da412a9b07d5cb6a4042360d29da2b442a2e
Heloise Dafforne <17DaffoH@fulford.york.sch.uk>
2022-09-12 14:55:29 +0100

    Add project files.

---
+++ new/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @new
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // repeat 24 times
+            for (int i = 0; i < 24; i++)
+            {
+                Console.WriteLine($"Give me {i} more!");
+            }
+
+            // repeat until user inputs yes
+            bool areWeNearlyThereYet = false;
+
+            while (areWeNearlyThereYet == false)
+            {
+                Console.WriteLine("Are we nearly there yet? ");
+                areWeNearlyThereYet = Console.ReadLine().ToLower() == "yes";
+            }
+        }
+    }
+}
