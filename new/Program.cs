bdd80f7406660eeba80640acb74053be8e2bcc8d
Heloise Dafforne <17DaffoH@fulford.york.sch.uk>
2022-09-12 15:16:22 +0100

    added code

--- new/Program.cs
+++ new/Program.cs
@@ -24,6 +24,30 @@ namespace @new
                 Console.WriteLine("Are we nearly there yet? ");
                 areWeNearlyThereYet = Console.ReadLine().ToLower() == "yes";
             }
+
+            // repeats from 1 to 50
+            for (int i = 0; i < 50; i++)
+            {
+                //say if number is even or odd
+                string oddOrEven = "odd";
+                if (i % 2 == 0)
+                {
+                    oddOrEven = "even";
+                }
+                Console.WriteLine($"{i} is {oddOrEven}");
+            }
+            
+             // count to 50
+            for (int i = 0; i < 50; i++)
+            {
+                //say if number is divisible
+                string divByThree = "divisible by three";
+                if (i % 3 != 0)
+                    {
+                        divByThree = "not divisible by three";
+                    }
+                    Console.WriteLine($"{i} is {divByThree}");
+            }
         }
     }
 }
